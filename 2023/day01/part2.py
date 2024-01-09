99802a5b8a847e2da6c5f319336eb53802376ffd
dedolence <nhahoyt@gmail.com>
2024-01-08 19:06:45 -0500

    cleanup

--- 2023/day01/part2.py
+++ 2023/day01/part2.py
@@ -7,37 +7,27 @@
 
 filename = "input.txt"
 values = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-
-"""
-two1nine            = 2 + 9 = 11
-eightwothree        = 8 + 3 = 11
-abcone2threexyz     = 1 + 3 = 4
-xtwone3four         = 2 + 4 = 6
-4nineeightseven2    = 4 + 2 = 6
-zoneight234         = 1 + 4 = 5
-7pqrstsixteen       = 7 + 6 = 7
-"""
-
+pairs = []
 
 # format input
 input = []
 with open(filename) as file:
     input = [line.strip() for line in file.readlines()]
 
-#input = ["8zfgtfnxvjjxgptxkpkdb1gkndcsbgvzxgqg1oneightq"]
-
-pairs = []
 
 for line in input:
     l = len(line)
-    ds = []
+    digits = []
+    
     for i,c in enumerate(line):
         if line[i].isdigit():
-            ds.append(line[i])
+            digits.append(line[i])
+
         else:
             for k in values.keys():
                 if line[i:].startswith(k):
-                    ds.append(values[k])
-    pairs.append(int(f"{ds[0]}{ds[-1]}"))
+                    digits.append(values[k])
+
+    pairs.append(int(f"{digits[0]}{digits[-1]}"))
 
 print(sum(pairs))
