cd48b65b155bc91a0696b7b6f6bad5ad07d406c2
Nathaniel <nhahoyt@gmail.com>
2024-12-29 17:58:27 -0500

    day 1 part 2

--- 2024/day01/part1.py
+++ 2024/day01/part1.py
@@ -6,12 +6,35 @@ input = """
 3   9
 3   3
 """.strip()
+"""
+1   1000
+10  2
 
+1 + 990 = 991
 
-a = []
-b = []
-for line in input.split("\n"):
-    a.append(int(line[0]))
-    b.append(int(line[-1]))
+999 + (-8) = 991
+"""
 
-a, b = sorted(a), sorted(b)
+"""
+    Parse two columns into two lists.
+    Order both lists ascending.
+    Find difference between values at index n of each list.
+    Answer: sum total of differences.
+"""
+column_a = []
+column_b = []
+
+with open("input.txt") as file:
+    line_counter = 0
+    for line in file.readlines():
+        a, b = line.split("   ")
+        column_a.append(int(a))
+        column_b.append(int(b))
+        
+a, b = sorted(column_a), sorted(column_b)
+
+paired = list(zip(a, b))
+
+differences = map(lambda d: abs(d[1] - d[0]), paired)
+
+print(sum(list(differences)))
