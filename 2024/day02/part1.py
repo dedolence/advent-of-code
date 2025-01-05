e9713a9b1e11577b51d69ce5e00365ffd9314f9b
Nathaniel <nhahoyt@gmail.com>
2025-01-05 00:25:44 -0500

    day 2 part 1

---
+++ 2024/day02/part1.py
@@ -0,0 +1,31 @@
+
+def check_test(test, direction=None):
+    if len(test) == 1:
+        return True
+    
+    a, b = test[0], test[1]
+
+    dif = abs(a - b)
+    if dif < 1 or dif > 3:
+        return False
+    
+    if direction == None:
+        # False evaluates to 0, True to 1, so they can be used as indices
+        direction = ("<", ">")[a - b > 0]
+
+    if eval(f"{a}{direction}{b}"):
+        return check_test(test[1:], direction)
+    else:
+        return False
+
+
+
+with open("input.txt") as input:
+    safe_tests = 0
+    tests = [line.strip() for line in input.readlines()]
+    for test in tests:
+        test = [int(i) for i in test.split(" ")]
+        if check_test(test):
+            safe_tests += 1
+    
+    print(safe_tests)
