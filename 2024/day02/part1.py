45173718b8e5b1ceff5e8134777e2df9f4844112
Nathaniel <nhahoyt@gmail.com>
2025-01-05 00:29:11 -0500

    doc

--- 2024/day02/part1.py
+++ 2024/day02/part1.py
@@ -14,7 +14,7 @@ def check_test(test, direction=None):
         direction = ("<", ">")[a - b > 0]
 
     if eval(f"{a}{direction}{b}"):
-        return check_test(test[1:], direction)
+        return check_test(test[1:], direction)  # direction is consistent, call again with next two
     else:
         return False
 
@@ -24,7 +24,7 @@ with open("input.txt") as input:
     safe_tests = 0
     tests = [line.strip() for line in input.readlines()]
     for test in tests:
-        test = [int(i) for i in test.split(" ")]
+        test = [int(i) for i in test.split(" ")]    # convert to integers
         if check_test(test):
             safe_tests += 1
     
