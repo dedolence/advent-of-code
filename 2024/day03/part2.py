a39cbcf3ec239b53a0533555e6621e4651166b6a
Nathaniel <nhahoyt@gmail.com>
2025-02-01 20:20:52 -0500

    start day 4

--- 2024/day03/part2.py
+++ 2024/day03/part2.py
@@ -7,7 +7,7 @@ the regex function from part 1's solution.
 
 import re
 
-input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+#input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 def reduce_instructions(input: str):
     for i in range(len(input)):
---
+++ 2024/day04/part1.py
@@ -0,0 +1,24 @@
+
+
+with open("test.txt") as file:
+    input = [line.strip() for line in file.readlines()]
+
+def horizontal(input):
+    return "".join(input)
+
+def vertical(input):
+    vert = []
+    s = ""
+    for i, col in enumerate(input[0]):
+        for row in input:
+            s += row[i]
+        vert.append(s)
+        s = ""
+    return "".join(vert)
+
+
+def count_xmases(input: str):
+    return input.count("XMAS") + input.count("SAMX")
+
+print("Horizontal XMASes: ", count_xmases(horizontal(input)))
+print("Vertical XMASes: ", count_xmases(vertical(input)))
