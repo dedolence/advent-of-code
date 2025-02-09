8d9b1f6f76b8e8f44b3577e9316a155c2048294d
Nathaniel <nhahoyt@gmail.com>
2025-02-09 01:11:25 -0500

    day 4 part 1 done

--- 2024/day04/part1.py
+++ 2024/day04/part1.py
@@ -1,27 +1,38 @@
+with open("input.txt") as file:
+    # because python allows negative index values we need to add a border
+    # of "." all the way around to prevent wraparound false-positives.
+    input = ["." + line.strip() + "." for line in file.readlines()]
+    pad_row = "." * len(input[0])
+    input = [pad_row] + input + [pad_row]
 
 
-with open("test.txt") as file:
-    input = [line.strip() for line in file.readlines()]
+def check(coords, direction):
+    row, col = coords 
+    x, y = direction
 
-def horizontal(input):
-    return "".join(input)
-
-def vertical(input):
-    vert = []
-    s = ""
-    for i, col in enumerate(input[0]):
-        for row in input:
-            s += row[i]
-        vert.append(s)
-        s = ""
-    return "".join(vert)
-
-def diagonal(input):
-    
+    def rec(i = 0, s = ""):
+        if i == 4: return s
+        else:
+            try:
+                s += input[row + y * i][col + x * i]    # shoutout PEMDAS
+            except IndexError:
+                return False
+            return rec(i + 1, s)
+        
+    return rec() == "XMAS"
 
 
-def count_xmases(input: str):
-    return input.count("XMAS") + input.count("SAMX")
+total = 0
+for r, row in enumerate(input):
+    for c, char in enumerate(row):
+        total += check((r, c), (0, 1))      # i actually hate the repetition here
+        total += check((r, c), (0, -1))     # (and i hate nested for loops)
+        total += check((r, c), (1, 0))      # but at the moment i can't think of 
+        total += check((r, c), (-1, 0))     # a way that avoids it.
+        total += check((r, c), (1, 1))
+        total += check((r, c), (1, -1))
+        total += check((r, c), (-1, -1))
+        total += check((r, c), (-1, 1))
 
-print("Horizontal XMASes: ", count_xmases(horizontal(input)))
-print("Vertical XMASes: ", count_xmases(vertical(input)))
+
+print(total)
